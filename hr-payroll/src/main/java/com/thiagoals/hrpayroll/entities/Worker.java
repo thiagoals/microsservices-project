c2dd1a3d052c205b14c28fb27fbc7c7f82419db1
Thiago Augusto Lopes Silva <4583@CEUMA.EDU.BR>
2021-01-29 19:13:57 -0300

    Adding request of payroll to worker

---
+++ hr-payroll/src/main/java/com/thiagoals/hrpayroll/entities/Worker.java
@@ -0,0 +1,17 @@
+package com.thiagoals.hrpayroll.entities;
+
+import java.io.Serializable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Worker implements Serializable{
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String name;
+	private double dailyIncome;
+}
